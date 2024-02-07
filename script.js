37fae4664d83f9cadab64728db944bf9b5d80c77
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-02-07 13:03:42 +0100

    Style page

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const quote = document.querySelector(".quote");
 const owner = document.querySelector(".author");
 const reloadBtn = document.querySelector(".reload");
 reloadBtn.addEventListener("click", randomQuote);
+window.addEventListener("DOMContentLoaded", randomQuote)
 async function randomQuote() {
   const data = await fetch("https://api.quotable.io/random");
   const display = await data.json();
   const { content, author } = display;
-  quote.textContent = content;
+  quote.textContent =  content;
   owner.textContent = author;
   console.log(display)
 }
